526d4f386f300dae2d61c149204d56c95cfb1d35
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-10-19 11:04:45 +0800

    scrapper.py: finds a single item name

--- scrapper.py
+++ scrapper.py
@@ -3,16 +3,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == '__main__':
     url = 'https://shopee.tw/search?keyword=nvidia'
 
-    options = Options()
-    options.headless = True
-    options.add_argument("--window-size=1920,1080")
-
-    driver = webdriver.Chrome(options=options)
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
 
-    main = driver.find_element_by_id('main')
-    driver.quit()
+    item_list = WebDriverWait(driver, 10).until(EC.visibility_of_element_located( \
+      (By.XPATH, "//div[contains(@class, 'shopee-search-item-result__items')]")))
+
+    print("Finished Waiting")
+
+    items = item_list.find_elements_by_class_name('shopee-search-item-result__item')
+
+    for item in items:
+        name = item.find_element_by_xpath('//div[@data-sqe="name"]//div')
+        print(name.text)
+
+  return 0
