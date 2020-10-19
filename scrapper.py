99fec16ebbb83b938f30705d60d39c6bd9db0922
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-10-19 14:51:11 +0800

    scrapper.py: able to wait and load all items in first search page

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from time import sleep
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
@@ -11,9 +13,24 @@ if __name__ == '__main__':
     url = 'https://shopee.tw/search?keyword=nvidia'
 
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
+
+    WebDriverWait(driver, 10).until(EC.visibility_of_any_elements_located( \
+      (By.CLASS_NAME, 'shopee-search-item-result__item')))
+
+    height = driver.execute_script("return document.documentElement.scrollHeight")
+    i = 0
+    while i < height:
+        print("i = {}".format(i))
+        height = driver.execute_script("return document.documentElement.scrollHeight")
+        print("height = {}".format(height))
+        driver.execute_script("window.scrollTo(0, {});".format(i))
+        i = i + 500
+        sleep(0.5)
+
 
     items = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located( \
       (By.CLASS_NAME, 'shopee-search-item-result__item')))
