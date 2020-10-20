7c28a17bbfc6cd16cc91e534140d9242699ce811
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-10-20 11:30:10 +0800

    psql_utils.py: changed scrapper_db to scraper_db

--- psql_utils.py
+++ psql_utils.py
@@ -3,7 +3,7 @@ from getpass import getpass
 import psycopg2
 
 def InitializePostgreSQLDatabase(
-  user: str, db_name: str='scrapper_db', host: str='localhost') -> None :
+  user: str, db_name: str='scraper_db', host: str='localhost') -> None :
     password = getpass(prompt='Password of user: ')
 
     connect_string = 'dbname={} user={} host={} password={}'.format(
