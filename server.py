8ed50834f905a6b4a98104f3474f0b9663592e33
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-11-02 11:10:59 +0800

    server.py: index.html displays list of search phrases

--- server.py
+++ server.py
@@ -4,6 +4,11 @@ from flask import render_template
 from psql_utils import ConnectDatabase
 from psql_utils import SelectAllShopeeItems
 
+from sqlalchemy import distinct
+from sqlalchemy.orm import sessionmaker
+
+from models import ShopeeItem
+
 from base64 import b64encode
 
 app = Flask(__name__)
@@ -12,7 +17,14 @@ engine = ConnectDatabase(user='d400')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    table = ShopeeItem.__table__.name
+    search_phrases = [r for (r,) in session.query(ShopeeItem.search_phrase).distinct()]
+    for search_phrase in search_phrases:
+        print("search_phrase: {}".format(search_phrase))
+        print("Type of search_phrase: {}".format(type(search_phrase)))
+    return render_template("index.html", search_phrases=search_phrases)
 
 @app.route('/hello')
 def hello():
